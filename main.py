db74b8d5dd22c9592b2ea54efb38843fbe212049
Vinitha Harith <vinitha.harith@gmail.com>
2023-03-03 14:09:12 +0100

    geo api

--- main.py
+++ main.py
@@ -24,23 +24,23 @@ client = get_client(project_id)
 
 @app.route('/geo')
 def update_visitor_loc():
-    tmp = str(request.environ.get('HTTP_X_FORWARDED_FOR'))
+    client_ip = str(request.environ.get('HTTP_X_FORWARDED_FOR'))
     # client_ip, fwd_ip = tmp.split(",")
 
     tmp1 = str(request.environ.get('X-client-Geo-region'))
-    region = request.headers.get('X-client-Geo-region')
-    city = request.headers.get('X-client-Geo-city')
+    client_region = request.headers.get('X-client-Geo-region')
+    client_city = request.headers.get('X-client-Geo-city')
     # client_region, client_city = tmp1.split(",")
 
-    # key_v = client.key('Visitors-loc')
-    # visitors_loc = datastore.Entity(key_v)
-    # visitors_loc["client_city"] = client_city
-    # visitors_loc["client_region"] = client_region
-    # visitors_loc["update_ts"] = datetime.now()
-    # visitors_loc["client_ip"] = client_ip
-    # client.put(visitors_loc)
+    key_v = client.key('Visitors-loc')
+    visitors_loc = datastore.Entity(key_v)
+    visitors_loc["client_city"] = client_city
+    visitors_loc["client_region"] = client_region
+    visitors_loc["update_ts"] = datetime.now()
+    visitors_loc["client_ip"] = client_ip
+    client.put(visitors_loc)
 
-    return ({"region": region}, 200, CORS_HEADERS)
+    return ({"region": client_city}, 200, CORS_HEADERS)
 
 
 @ app.route('/')
