1ae513286bf2983fa4577debd70ca27c7a8e4476
Vinitha Harith <vinitha.harith@gmail.com>
2023-03-04 19:06:57 +0100

    lat long

--- main.py
+++ main.py
@@ -30,12 +30,16 @@ def update_visitor_loc():
     tmp1 = str(request.environ.get('X-client-Geo-region'))
     client_region = request.headers.get('X-client-Geo-region')
     client_city = request.headers.get('X-client-Geo-city')
-    # client_region, client_city = tmp1.split(",")
+    client_place = str(request.headers.get('X-client-place'))
+    client_lat, client_long = client_place.split(",")
 
     key_v = client.key('Visitors-loc')
     visitors_loc = datastore.Entity(key_v)
     visitors_loc["client_city"] = client_city
     visitors_loc["client_region"] = client_region
+    visitors_loc["client_place"] = client_place
+    visitors_loc["client_lat"] = client_lat
+    visitors_loc["client_long"] = client_long
     visitors_loc["update_ts"] = datetime.now()
     visitors_loc["client_ip"] = client_ip
     client.put(visitors_loc)
